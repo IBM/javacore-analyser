5c87c649a068777f4347374774c16618c7a5dd34
Krzysztof Kaźmierczyk <kazm@ibm.com>
2024-10-30 18:10:33 +0100

    Ref #8 Upload and display uploaded dir
    Signed-off-by: Krzysztof Kaźmierczyk <kazm@ibm.com>

--- javacore_analyser_web.py
+++ javacore_analyser_web.py
@@ -3,11 +3,11 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 import os
+import re
 import tempfile
+from datetime import datetime
 
-# Assisted by WCA@IBM
-# Latest GenAI contribution: ibm/granite-20b-code-instruct-v2
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_from_directory, redirect
 
 import javacore_analyzer
 
@@ -15,10 +15,17 @@ REPORTS_DIR = "reports"
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/reports/<path:path>')
+def dir_listing(path):
+    return send_from_directory(REPORTS_DIR, path)
+
+# Assisted by WCA@IBM
+# Latest GenAI contribution: ibm/granite-20b-code-instruct-v2
 @app.route('/upload', methods=['POST'])
 def upload_file():
     # Create a temporary directory to store uploaded files
@@ -35,8 +42,19 @@ def upload_file():
         file.save(file_name)
         input_files.append(file_name)
 
+    if len(input_files) == 1:
+        report_name = input_files[0]
+    else:
+        report_name = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    report_name = re.sub(r'[^a-zA-Z0-9]', '_', report_name)
+
     # Process the uploaded file
-    javacore_analyzer.process_javacores_and_generate_report_data(input_files, REPORTS_DIR)
+    report_output_dir = REPORTS_DIR + '/' + report_name
+    javacore_analyzer.process_javacores_and_generate_report_data(input_files, report_output_dir)
+
+    return redirect(report_output_dir + "/index.html")
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
