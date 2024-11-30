f7b25f7d19a6604ae53c99b39679ee70abbd62bf
Krzysztof Kazmierczyk <kazm@ibm.com>
2024-11-30 14:09:55 +0100

    Ref #27 Fix  Uncontrolled data used in path expression in javacore_analyser_web.py
    Signed-off-by: Krzysztof Kaźmierczyk <kazm@ibm.com>

--- src/javacore_analyser/javacore_analyser_web.py
+++ src/javacore_analyser/javacore_analyser_web.py
@@ -99,9 +99,12 @@ def upload_file():
 
         # Create a temporary directory to store uploaded files
         # Note We have to use permanent files and then delete them.
-        # tempfile.Temporary directory function does not work when you want to access files from another threads.
-        javacores_temp_dir_name = os.path.join(reports_dir, TEMP_DIR, report_name)
+        # tempfile.Temporary_directory function does not work when you want to access files from another threads.
+        javacores_temp_dir_name = os.path.normpath(os.path.join(reports_dir, TEMP_DIR, report_name))
+        if not javacores_temp_dir_name.startswith(reports_dir):
+            raise Exception("Security exception: Uncontrolled data used in path expression")
         javacores_temp_dir = os.mkdir(javacores_temp_dir_name)
+
 
         # Get the list of files from webpage
         files = request.files.getlist("files")
