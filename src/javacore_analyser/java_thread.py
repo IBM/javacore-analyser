64f071b92a024b98acadc9250d8731c2e246104b
Krzysztof Kaźmierczyk <kazm@ibm.com>
2025-02-24 13:42:18 +0100

    Ref #126 updated disclaimers
    Signed-off-by: Krzysztof Kaźmierczyk <kazm@ibm.com>

--- src/javacore_analyser/java_thread.py
+++ src/javacore_analyser/java_thread.py
@@ -1,9 +1,9 @@
 #
-# Copyright IBM Corp. 2024 - 2024
+# Copyright IBM Corp. 2024 - 2025
 # SPDX-License-Identifier: Apache-2.0
 #
-from javacore_analyser.properties import Properties
 from javacore_analyser.abstract_snapshot_collection import AbstractSnapshotCollection
+from javacore_analyser.properties import Properties
 
 
 class Thread(AbstractSnapshotCollection):
--- test/test_properties.py
+++ test/test_properties.py
@@ -1,3 +1,7 @@
+#
+# Copyright IBM Corp. 2024 - 2025
+# SPDX-License-Identifier: Apache-2.0
+#
 import logging
 import unittest
 
