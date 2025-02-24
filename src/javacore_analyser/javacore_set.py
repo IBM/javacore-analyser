d7f204788070520388fb1122e086e92e9e59db08
Krzysztof Kaźmierczyk <kazm@ibm.com>
2025-02-24 14:04:20 +0100

    Ref #126 changed logging level
    Signed-off-by: Krzysztof Kaźmierczyk <kazm@ibm.com>

--- src/javacore_analyser/javacore_set.py
+++ src/javacore_analyser/javacore_set.py
@@ -20,12 +20,12 @@ from lxml.etree import XMLSyntaxError
 from tqdm import tqdm
 
 from javacore_analyser import tips
-from javacore_analyser.properties import Properties
 from javacore_analyser.code_snapshot_collection import CodeSnapshotCollection
 from javacore_analyser.constants import *
 from javacore_analyser.har_file import HarFile
 from javacore_analyser.java_thread import Thread
 from javacore_analyser.javacore import Javacore
+from javacore_analyser.properties import Properties
 from javacore_analyser.snapshot_collection import SnapshotCollection
 from javacore_analyser.snapshot_collection_collection import SnapshotCollectionCollection
 from javacore_analyser.verbose_gc import VerboseGcParser
@@ -52,7 +52,7 @@ def _create_xml_xsl_for_collection(tmp_dir, templates_dir, xml_xsl_filename, col
                 f.write(file_content.format(id=element_id))
                 f.close()
             else:
-                logging.info("Skipping boring file: " + filename)
+                logging.debug("Skipping boring file: " + filename)
 
 
 class JavacoreSet:
