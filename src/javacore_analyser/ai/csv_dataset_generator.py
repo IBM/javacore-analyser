1c69c844fd2fba97ac32a96ec81e393abe1104cb
Krzysztof Kaźmierczyk <kazm@ibm.com>
2025-08-20 13:53:41 +0200

    Fixes #160 Generating learning dataset
    Signed-off-by: Krzysztof Kaźmierczyk <kazm@ibm.com>
    Signed-off-by: Piotr Anioła <piotr.aniola@ibm.com>

---
+++ src/javacore_analyser/ai/csv_dataset_generator.py
@@ -0,0 +1,34 @@
+import sys
+
+import pandas as pd
+
+from javacore_analyser.javacore_analyser_batch import generate_javecore_set_data
+
+
+def main(input_files):
+    javacore_set = generate_javecore_set_data(input_files)
+    data = []
+    for thread in javacore_set.threads:
+        for snapshot in thread.thread_snapshots:
+            name = snapshot.name
+            cpu_usage = snapshot.cpu_usage
+            allocated_mem = snapshot.allocated_mem
+            state = snapshot.state
+            blocking_threads = len(snapshot.blocking)
+            stack_trace = snapshot.stack_trace
+            if stack_trace is None:
+                stack_trace = ""
+            else:
+                stack_trace = stack_trace.to_string()
+            data.append({'name': name, 'cpu_usage': cpu_usage, 'allocated_mem': allocated_mem, 'state': state,
+                         'blocking_threads': blocking_threads, 'stack_trace': stack_trace})
+    pd.DataFrame.from_records(data).to_csv('data.csv', index=False)
+
+    # Check if we can load data
+    df = pd.read_csv('data.csv')
+    print("Displaying data:")
+    print(df)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
