9997d5863cfa1c3039841803d1cd7cb5f3abcb9f
Krzysztof Kaźmierczyk <kazm@ibm.com>
2026-07-23 14:51:13 +0200

    Ref #258 Switch to execute javacore_analyser from javacore_analyser_batch.py
    Signed-off-by: Krzysztof Kaźmierczyk <kazm@ibm.com>

--- src/javacore_analyser/javacore_analyser_batch.py
+++ src/javacore_analyser/javacore_analyser_batch.py
@@ -20,7 +20,7 @@ import py7zr
 from importlib_resources.abc import Traversable
 
 from javacore_analyser import common_utils
-from javacore_analyser.javacore_set import JavacoreSet
+from javacore_analyser.javacore_analyzer import JavacoreAnalyzer
 from javacore_analyser.properties import Properties
 
 SUPPORTED_ARCHIVES_FORMATS = {"zip", "tar", "gz", "tgz", "bz2", "lzma", "7z"}
@@ -164,7 +164,7 @@ def generate_javecore_set_data(files):
                     extract_archive(file, javacores_temp_dir_name)  # Extract archive to temp dir
                 else:
                     shutil.copy2(file, javacores_temp_dir_name)
-        return JavacoreSet.process_javacores(javacores_temp_dir_name)
+        return JavacoreAnalyzer.process_javacores(javacores_temp_dir_name)
     finally:
         javacores_temp_dir.cleanup()
 
