e1bb3575d244628cfd1080499710d0be10df00e6
Krzysztof Kaźmierczyk <kazm@ibm.com>
2026-07-23 21:05:39 +0200

    Fixes #19 Refactor Javascript
    Signed-off-by: Krzysztof Kaźmierczyk <kazm@ibm.com>

--- src/javacore_analyser/data/style.js
+++ src/javacore_analyser/data/style.js
@@ -3,6 +3,10 @@ Copyright IBM Corp. 2025 - 2026
 SPDX-License-Identifier: Apache-2.0
 */
 
+// 'use strict' opts this file into strict mode: undeclared variables, duplicate
+// parameter names, and other silent JavaScript mistakes become hard errors.
+'use strict';
+
 $(function(){
     $('#generated_reports_table').tablesorter({
         theme : 'blue',
