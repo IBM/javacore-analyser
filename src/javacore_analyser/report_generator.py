b9d8bb9f622aad6a053084f8ec93f6ac200fdec5
Piotr Anioła <piotr.aniola@ibm.com>
2026-09-14 15:35:02 +0200

    Ref #0 Use with-statement to guarantee file close in _create_xml_xsl_for_collection
    Signed-off-by: Piotr Anioła <piotr.aniola@ibm.com>

--- src/javacore_analyser/report_generator.py
+++ src/javacore_analyser/report_generator.py
@@ -153,9 +153,8 @@ class ReportGenerator:
                 if element.is_interesting() or not Properties.get_instance().skip_boring():
                     file = os.path.join(tmp_dir, filename)
                     logging.debug("Writing file " + file)
-                    f = open(file, "w")
-                    f.write(file_content.format(id=element_id))
-                    f.close()
+                    with open(file, "w") as f:
+                        f.write(file_content.format(id=element_id))
                 else:
                     logging.debug("Skipping boring file: " + filename)
 
