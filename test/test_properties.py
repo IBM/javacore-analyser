ff526e3bea79ddd66a8b84cc610097b27eb509be
Piotr Anioła <piotr.aniola@ibm.com>
2026-09-16 11:54:01 +0200

    Ref #430 Deliberately broken test to verify CI triggers on PR merge
    Signed-off-by: Piotr Anioła <piotr.aniola@ibm.com>

--- test/test_properties.py
+++ test/test_properties.py
@@ -17,7 +17,7 @@ class TestProperties(unittest.TestCase):
         instance = Properties.get_instance()
         self.assertIsNotNone(instance, "Properties instance should not be None")
         instance2 = Properties.get_instance()
-        self.assertEqual(instance, instance2, "Singleton test failure")
+        self.assertNotEqual(instance, instance2, "Singleton test failure")
 
     def test_init(self):
         # it should not be possible to create an object by calling the constructor
